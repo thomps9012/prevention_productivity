a3068f86952d8d6544dbc426b0bc8b23ab3a8a4c
Samuel Thompson <thomps9012@gmail.com>
2022-04-27 08:59:25 -0400

    added in base level contact CRUD

---
+++ Pages/Contacts/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using prevention_productivity.Data;
+using prevention_productivity.Models;
+
+namespace prevention_productivity.Pages.Contacts
+{
+    public class CreateModel : PageModel
+    {
+        private readonly prevention_productivity.Data.ApplicationDbContext _context;
+
+        public CreateModel(prevention_productivity.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Contact Contact { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Conttact.Add(Contact);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
