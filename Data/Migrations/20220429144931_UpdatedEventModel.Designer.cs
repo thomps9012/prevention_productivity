eb3ac4cee69ddf3f4ce0332264b9a4aa1bc6a691
Samuel Thompson <thomps9012@gmail.com>
2022-04-29 10:50:20 -0400

    added db migration to reflect new event model

--- Data/Migrations/20220429144931_UpdatedEventModel.Designer.cs
+++ Data/Migrations/20220429144931_UpdatedEventModel.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using prevention_productivity.Data;
 
@@ -11,9 +12,10 @@ using prevention_productivity.Data;
 namespace prevention_productivity.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220429144931_UpdatedEventModel")]
+    partial class UpdatedEventModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
