7f52a9012981dc70adb4d2c43ec4bd11b2ef5f31
Samuel Thompson <thomps9012@gmail.com>
2022-04-29 09:35:16 -0400

    update register page

--- Pages/Privacy.cshtml.cs
+++ Pages/Privacy.cshtml.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Authorization;
 
 namespace prevention_productivity.Pages
 {
+    [AllowAnonymous]
     public class PrivacyModel : PageModel
     {
         private readonly ILogger<PrivacyModel> _logger;
