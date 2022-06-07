4156f9294b9bbbf6e4c73ff087d3e8a9dac25d2b
Samuel Thompson <thomps9012@gmail.com>
2022-06-07 16:03:39 -0400

    updated model creation types

--- graph/model/models_gen.go
+++ graph/model/models_gen.go
@@ -32,22 +32,22 @@ type LoginInput struct {
 }
 
 type NewLog struct {
-	FocusArea    string `json:"focus_area"`
+	FocusArea    string `json:"focus_area" bson:"focus_area"`
 	Actions      string `json:"actions"`
 	Successes    string `json:"successes"`
 	Improvements string `json:"improvements"`
-	NextSteps    string `json:"next_steps"`
+	NextSteps    string `json:"next_steps" bson:"next_steps"`
 }
 
 type NewNote struct {
-	ItemID  string `json:"item_id"`
+	ItemID  string `json:"item_id" bson:"item_id"`
 	Title   string `json:"title"`
 	Content string `json:"content"`
 }
 
 type NewUser struct {
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
+	FirstName string `json:"first_name" bson:"first_name"`
+	LastName  string `json:"last_name" bson:"last_name"`
 	Email     string `json:"email"`
 	Password  string `json:"password"`
 }
@@ -96,9 +96,9 @@ type User struct {
 	Email     string  `json:"email"`
 	Username  string  `json:"username"`
 	Password  string  `json:"password"`
-	IsAdmin   bool    `json:"is_admin"`
 	CreatedAt string  `json:"created_at" bson:"created_at"`
 	UpdatedAt string  `json:"updated_at" bson:"updated_at"`
 	DeletedAt string `json:"deleted_at" bson:"deleted_at"`
+	IsAdmin   bool    `json:"is_admin"`
 	IsActive  bool    `json:"is_active"`
 }
