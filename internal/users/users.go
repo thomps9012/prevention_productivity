0558b617ee7a5d12cf9f98a38ed38ee08bc6aafe
Samuel Thompson <thomps9012@gmail.com>
2022-07-13 10:31:22 -0400

    updated admin permissions

--- internal/users/users.go
+++ internal/users/users.go
@@ -1,28 +1,28 @@
 package users
 
 import (
-	database "thomps9012/prevention_productivity/internal/db"
-	"go.mongodb.org/mongo-driver/bson"
-	"golang.org/x/crypto/bcrypt"
 	"context"
 	"fmt"
-	"github.com/google/uuid"
+	database "thomps9012/prevention_productivity/internal/db"
 	"time"
+
+	"github.com/google/uuid"
+	"go.mongodb.org/mongo-driver/bson"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type User struct {
-	ID	   string `json:"id" bson:"_id"`
+	ID        string `json:"id" bson:"_id"`
 	FirstName string `json:"firstName" bson:"first_name"`
-	LastName string `json:"lastName" bson:"last_name"`
-	Email string `json:"email" bson:"email"`
-	Password string `json:"password" bson:"password"`
-	IsAdmin bool `json:"isAdmin"`
-	IsActive bool `json:"isActive"`
+	LastName  string `json:"lastName" bson:"last_name"`
+	Email     string `json:"email" bson:"email"`
+	Password  string `json:"password" bson:"password"`
+	IsAdmin   bool   `json:"isAdmin"`
+	IsActive  bool   `json:"isActive"`
 	CreatedAt string `json:"created_at" bson:"created_at"`
 	UpdatedAt string `json:"updated_at" bson:"updated_at"`
 	DeletedAt string `json:"deleted_at" bson:"deleted_at"`
 }
-
 
 func (u *User) Create() {
 	collection := database.Db.Collection("users")
@@ -72,7 +72,7 @@ func HashPassword(password string) (string, error) {
 func CheckPasswordHash(hash, password string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	if err != nil {
-	println(err.Error())
+		println(err.Error())
 	}
 	return err == nil
 }
@@ -81,27 +81,27 @@ func (u *User) Update(id string) {
 	collection := database.Db.Collection("users")
 	filter := bson.D{{"_id", id}}
 	u.UpdatedAt = time.Now().Format("2006-01-02 15:04:05")
-	if(len(u.Password) <= 0) {
+	if len(u.Password) <= 0 {
 		println("not updating pw")
 		fmt.Printf("%v\n", u)
-		result, err := collection.UpdateOne(context.TODO(), filter, bson.D{{"$set", bson.M{"first_name": u.FirstName, "last_name": u.LastName, "email": u.Email, "updated_at": u.UpdatedAt}}})
+		result, err := collection.UpdateOne(context.TODO(), filter, bson.D{{"$set", bson.M{"first_name": u.FirstName, "last_name": u.LastName, "email": u.Email, "updated_at": u.UpdatedAt, "is_active": u.IsActive, "is_admin": u.IsAdmin}}})
 		if err != nil {
 			panic(err)
 		}
 		println(result.ModifiedCount)
 	} else {
-			
-			hashed, hashErr := HashPassword(u.Password)
-			u.Password = hashed
-			println("update pw", u.Password)
-			fmt.Printf("%v\n", u)
-			if hashErr != nil {
-				fmt.Println(hashErr)
-			}
-			result, err := collection.UpdateOne(context.TODO(), filter, bson.D{{"$set", u}})
-			if err != nil {
-				panic(err)
-			}
+
+		hashed, hashErr := HashPassword(u.Password)
+		u.Password = hashed
+		println("update pw", u.Password)
+		fmt.Printf("%v\n", u)
+		if hashErr != nil {
+			fmt.Println(hashErr)
+		}
+		result, err := collection.UpdateOne(context.TODO(), filter, bson.D{{"$set", u}})
+		if err != nil {
+			panic(err)
+		}
 		println(result.ModifiedCount)
 	}
 }
