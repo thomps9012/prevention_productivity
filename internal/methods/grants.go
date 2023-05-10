6f269d6fed6905cfa8b82010bcd7c221e347b51a
Samuel Thompson <thomps9012@gmail.com>
2023-05-10 15:39:14 -0400

    rough build out of grant methods

--- internal/methods/grants.go
+++ internal/methods/grants.go
@@ -9,14 +9,43 @@ import (
 
 	"github.com/google/uuid"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func FindGrantDetail(grant_id string) (*model.GrantDetail, error) {
-	return nil, errors.New("method unimplemented")
+	collection := database.Db.Collection("grants")
+	user_stage := bson.D{{Key: "$lookup", Value: bson.D{{Key: "from", Value: "users"}, {Key: "localField", Value: "created_by"}, {Key: "foreignField", Value: "_id"}, {Key: "as", Value: "created_by"}, {
+		// add unwinding
+		Key: "pipeline", Value: bson.A{
+			bson.D{{Key: "$project", Value: bson.D{{Key: "first_name", Value: 1}, {Key: "last_name", Value: 1}, {Key: "_id", Value: 1}}}},
+		},
+	}}}}
+	pipeline := mongo.Pipeline{bson.D{{Key: "_id", Value: grant_id}}, user_stage}
+	cursor, err := collection.Aggregate(context.TODO(), pipeline)
+	if err != nil {
+		return nil, err
+	}
+	var res *model.GrantDetail
+	err = cursor.All(context.TODO(), &res)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
 }
 func FindAllGrants() ([]*model.GrantOverview, error) {
-	return nil, errors.New("method unimplemented")
+	projection := options.Find().SetProjection(bson.D{{Key: "_id", Value: 1}, {Key: "name", Value: 1}, {Key: "start_date", Value: 1}, {Key: "end_date", Value: 1}, {Key: "award_date", Value: 1}, {Key: "award_number", Value: 1}, {Key: "budget", Value: 1}, {Key: "active", Value: 1}})
+	collection := database.Db.Collection("grants")
+	cursor, err := collection.Find(context.TODO(), bson.D{{}}, projection)
+	if err != nil {
+		return nil, err
+	}
+	var res []*model.GrantOverview
+	err = cursor.All(context.TODO(), &res)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
 }
 
 func CreateGrant(new_grant model.NewGrant, grant_creator string) (*model.GrantDetail, error) {
