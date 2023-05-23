454d4f4f0424fe28effeb6534c5ef85ec9969358
Samuel Thompson <thomps9012@gmail.com>
2023-05-23 16:19:58 -0400

    push

--- main.go
+++ main.go
@@ -17,14 +17,8 @@ import (
 	"github.com/gorilla/handlers"
 )
 
-const defaultPort = "8080"
-
 func main() {
 	port := os.Getenv("PORT")
-	if port == "" {
-		port = defaultPort
-	}
-
 	router := chi.NewRouter()
 	database.InitDB()
 	defer database.CloseDB()
